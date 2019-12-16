51fe727f20226e8ea94aa0963bb843d4ba713d5a
Andrew Rezanovich <and.rezanovich@gmail.com>
2019-12-17 00:15:26 +0200

    first task

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict"
+
+var par6 = document.getElementById('par-6');
+par6.innerHTML = 'Я не буду смеяться над Даниными шутками.';
+par6.style.color = '#622010';
+console.log(par6.innerHTML);
+
+var par1 = document.getElementById("par-1");
+par1.style.color = '#622010';
+
+var green = document.getElementById('greenPar');
+green.style.color = '#34712a';
+
+
+
+
+// var elem = ['as', 12, 'dsf'];
+// elem[1] = 'qqq';
+// console.log(elem);
+
+
+
